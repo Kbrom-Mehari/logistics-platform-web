4059860eaa1094160943bb4f137a033368d1155d
kbrom <kibrommehari121@gmail.com>
2026-06-22 21:57:46 +0300

    primary rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { Layout } from './components/Layout'; // 1. Import your new layout component
+import LandingLayout from "./layouts/LandingLayout.tsx"
+import FeaturesPage from "./pages/FeaturesPage.tsx";
+import LandingPage from "./pages/LandingPage.tsx";
+import DashboardLayout from "./layouts/DashboardLayout.tsx";
+import SolutionsPage from "./pages/SolutionsPage.tsx";
 
 function App() {
     return (
-        // 2. Wrap your app content inside the Layout component
-        <Layout>
-            <button className="bg-blue-600 text-white px-4 py-2 rounded">Create New Shipment</button>
-        </Layout>
+        <LandingLayout>
+            <SolutionsPage />
+        </LandingLayout>
     );
 }
 
