1415ccb3f3d1992e3aa5dcc037f6fb5d6dbede87
kbrom <kibrommehari121@gmail.com>
2026-06-25 22:33:21 +0300

    router component

---
+++ src/router.tsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter } from 'react-router-dom';
+import LandingLayout from "./layouts/LandingLayout.tsx";
+import LandingPage from "./pages/LandingPage.tsx";
+import SolutionsPage from "./pages/SolutionsPage.tsx";
+import FeaturesPage from "./pages/FeaturesPage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+import RegisterPage from "./pages/RegisterPage.tsx";
+import DashboardLayout from "./layouts/DashboardLayout.tsx";
+
+export const router = createBrowserRouter(
+    [
+        {
+            path: '/',
+            element: <LandingLayout />,
+            children: [
+                { index: true, element: <LandingPage /> },
+                { path: "solutions", element: <SolutionsPage /> },
+                { path: "features", element: <FeaturesPage /> },
+                { path: "login", element: <LoginPage /> },
+                { path: "register", element: <RegisterPage /> }
+            ]
+        },
+
+        {
+            path: "/dashboard",
+            element: <DashboardLayout />,
+            children: [
+                { index: true, element: <LandingPage /> },
+            ]
+        },
+        {
+            path: "*",
+            element: <div>404 - Logistics Hub Not Found</div>,
+        }
+    ]
+);
+
