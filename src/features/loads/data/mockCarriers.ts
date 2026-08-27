f87c60cda6be683f2d2f23750caa7dd21c2ebb8f
kbrom <kibrommehari121@gmail.com>
2026-08-27 23:18:08 +0300

    create: mock carriers data

---
+++ src/features/loads/data/mockCarriers.ts
@@ -0,0 +1,24 @@
+import type { Carrier } from "../types/LoadsPageTypes";
+
+export const mockCarriers: Carrier[] = [
+    {
+        id: "carrier-1",
+        name: "Trans ETH",
+    },
+    {
+        id: "carrier-2",
+        name: "Ethio Freight",
+    },
+    {
+        id: "carrier-3",
+        name: "Tigrai Logistics",
+    },
+    {
+        id: "carrier-4",
+        name: "Kbrom Logistics",
+    },
+    {
+        id: "carrier-5",
+        name: "Abebe Isuzu",
+    },
+];
